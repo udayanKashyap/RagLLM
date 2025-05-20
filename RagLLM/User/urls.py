5b934d25598fde425f9ce4a419c95cf7b9c94ad4
Udayan Kashyap <kashyapudayan@gmail.com>
2025-05-20 13:15:47 +0530

    Removed unwanted files marked in .gitignore

---
+++ RagLLM/User/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
+
+app_name = "User"
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+]
